acdba292cf63cb43d2c09cb40ca2f28e8a4f8237
Posttwo <posttwo@users.noreply.github.com>
2020-10-24 23:54:36 +0100

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -32,7 +32,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function getAuthUrl($state)
     {
         return $this->buildAuthUrlFromBase(
-            'https://discordapp.com/api/oauth2/authorize', $state
+            'https://discord.com/api/oauth2/authorize', $state
         );
     }
 
@@ -41,7 +41,7 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getTokenUrl()
     {
-        return 'https://discordapp.com/api/oauth2/token';
+        return 'https://discord.com/api/oauth2/token';
     }
 
     /**
@@ -50,7 +50,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function getUserByToken($token)
     {
         $response = $this->getHttpClient()->get(
-            'https://discordapp.com/api/users/@me', [
+            'https://discord.com/api/users/@me', [
             'headers' => [
                 'Authorization' => 'Bearer '.$token,
             ],
